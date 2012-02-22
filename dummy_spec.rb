3ddeb4811f4d5a5771622675df83f64ffe88bbdb
Duncan Bayne <dhgbayne@gmail.com>
2012-02-23 08:37:21 +1100

    Basic rage functionality.

--- dummy_spec.rb
+++ dummy_spec.rb
@@ -1,5 +1,30 @@
 describe "Dummy" do
-  it "does nothing" do
+  it "passes" do
     1.should eql(1)
   end
+
+  it "fails" do
+    1.should eql(2)
+  end
+
+  it "fails" do
+    1.should eql(2)
+  end
+
+  it "fails" do
+    1.should eql(2)
+  end
+
+  it "fails" do
+    1.should eql(2)
+  end
+
+  it "fails" do
+    1.should eql(2)
+  end
+
+  it "passes" do
+    1.should eql(1)
+  end
+
 end
--- my_formatter.rb
+++
@@ -1,8 +0,0 @@
-require 'rspec/core/formatters/progress_formatter'
-
-class MyFormatter < RSpec::Core::Formatters::ProgressFormatter
-  def dump_summary(duration, example_count, failure_count, pending_count)
-    super
-    output.print ("Awesome\n")
-  end
-end
